875f0c9f3fff3aab4e646349960e259f04b09807
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-10-11 19:24:17 +0300

    Add some logic to store count steps and decide avatar in use

--- app/models/avatar.rb
+++ app/models/avatar.rb
@@ -2,4 +2,18 @@ class Avatar < ActiveRecord::Base
 
   belongs_to :user
 
+  def choose_avatar
+
+    ## select nice stepcounts
+    steps = self.user.steps.where( 'created_at > ?', Time.now - 3.days ).sum( :steps )
+
+    ## choose avatar type
+    if steps < 100:
+      return {:foo => 500}
+    end
+
+    return {:foo => 500}
+
+  end
+
 end
