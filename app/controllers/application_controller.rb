d2e5f144ac2fb6203eae19a7572615031da467e3
Matti Nelimarkka <matti.nelimarkka@hiit.fi>
2014-10-11 11:07:53 +0300

    Make a dummy UI for creating a new user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  before_filter :current_user
+
+  def current_user
+    @current_user = User.find_by_id( session[:user_id] )
+  end
+
   def home
-    redirect_to '/users/1'
+    redirect_to @current_user and return if @current_user
+    render 'application/index'
   end
 end
